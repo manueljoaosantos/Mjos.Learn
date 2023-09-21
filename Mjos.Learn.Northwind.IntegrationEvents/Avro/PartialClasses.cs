26b217466eb14fcbb82802c2992eabeebb69076a
Manuel Santos <manueljoaosantos@gmail.com>
2023-09-21 15:41:17 +0100

    feat: Create project Mjos.Learn.Northwind.IntegrationEvents

---
+++ Mjos.Learn.Northwind.IntegrationEvents/Avro/PartialClasses.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace Mjos.Learn.Northwind.IntegrationEvents.Avro
+{
+    public partial class ProductCreated : INotification
+    {
+    }
+
+    public partial class ProductUpdated : INotification
+    {
+    }
+
+    public partial class ProductDeleted : INotification
+    {
+    }
+}
